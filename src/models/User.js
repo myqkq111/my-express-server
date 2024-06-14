3d2ab798d6c5dc6a1c26aed0edce01d029e22fff
JUNSEO CHOI <myqkq111@naver.com>
2024-06-14 16:47:16 +0900

    refreshToken add

--- src/models/User.js
+++ src/models/User.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema({
     username : { type: String, required: true, unique: true},
     password : { type: String, required: true },
-    age : { type: Number, required: true}
+    age : { type: Number, required: true},
+    refreshToken : { type: String}
 })
 
 const User = mongoose.model('User', UserSchema);
--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 import passport from 'passport';
-import jwt from 'jsonwebtoken';
+import jwt, { decode } from 'jsonwebtoken';
 import User from '../models/User.js';
+import { generateAccessToken, generateRefreshToken } from '../utils/authUtils.js';
 
 const router = Router();
 
@@ -13,25 +14,57 @@ router.post('/login', (req, res, next) => {
     if (!user) {
       return res.status(401).json({ message: 'Authentication failed' });
     }
-    req.logIn(user, {session: false}, (err) => {
+    req.logIn(user, {session: false}, async (err) => {
       if (err) {
         return next(err);
       }
 
-      const accessToken = jwt.sign({ id: user._id, username: user.username }, 'kitri_secret', { expiresIn: '10m' })
+      const accessToken = generateAccessToken(user);
+      const refreshToken = generateRefreshToken(user);
 
-      return res.status(200).json({ accessToken });
+      user.refreshToken = refreshToken;
+      await user.save();
+
+      return res.status(200).json({ accessToken, refreshToken });
     });
   })(req, res, next);
 });
 
-router.get('/logout', (req, res) => {
-  req.logout((err) => {
-    if (err) {
-      return next(err);
-    }
-    res.status(200).json({ message: 'Logged out successfully' });
-  });
+router.post('/refresh-token', async (req, res) => {
+  const { refreshToken } = req.body;
+  if (!refreshToken) return res.sendStatus(401);
+
+  try {
+    const user = await User.findOne({ refreshToken });    
+    if(!user) return res.sendStatus(403);
+
+    jwt.verify(refreshToken, 'kitri_secret2', (err, decode) => {
+      if (err) return res.sendStatus(403);
+
+      const accessToken = generateAccessToken(user);
+      return res.json({ accessToken });
+    })
+  } catch (err) {
+    return res.sendStatus(500);
+  }
+});
+
+
+router.post('/logout', async (req, res) => {
+  const { refreshToken } = req.body;
+  if (!refreshToken) return res.sendStatus(401);
+
+  try {
+    const user = await User.findOne({ refreshToken });    
+    if(!user) return res.sendStatus(403);
+
+    user.refreshToken = null;
+    await user.seva();
+
+    res.status(200).json({ message : 'Logged out successfully' });
+  } catch (err) {
+    return res.sendStatus(500);
+  }
 });
 
 router.post('/join', async (req, res) => {
